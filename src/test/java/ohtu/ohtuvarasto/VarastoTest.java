557f93bb5fec4235f46b6ecd801da1877ce07183
rajaro <jaro.ravila@gmail.com>
2017-03-14 12:09:00 +0200

    travis test

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -22,7 +22,7 @@ public class VarastoTest {
 
     @Test
     public void konstruktoriLuoTyhjanVaraston() {
-        assertEquals(19, varasto.getSaldo(), vertailuTarkkuus);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
     }
     @Test
     public void konstruktoriVaatiiOikeanTilavuuden() {
